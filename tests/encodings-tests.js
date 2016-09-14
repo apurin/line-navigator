b053567c75fd056598b7b70be947a8debbd39d0a
Anton Purin <anpur@microsoft.com>
2016-09-14 16:20:18 +0300

    Adding test files for encodings

---
+++ tests/encodings-tests.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var tmp = require('tmp');
+var assert = require("chai").assert;
+var lineNavigator = require('../line-navigator.js');
+
+describe("Encoding tests", function(){  
+    it("", function(){
+    });
+});
